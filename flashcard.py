0812932cbc85f9944bb5084f5d2ef20f53ac5956
Dave Compton <to.dave.c@gmail.com>
2009-04-22 13:23:39 -0700

    introduced known / working /unknown lists.

--- flashcard.py
+++ flashcard.py
@@ -22,53 +22,73 @@ def print_card(card) :
         print word, "      ",
     print ""
 
-print "Flash card script version 0.3 by Akkana Peck.\n"
-print "On seeing a word, think of the answer then hit return."
-print "Then hit return again to see the next word."
-print "But if you got it wrong, type anything besides q before hitting Return"
-print "and the word will be added to a list to be presented more often."
-print "q<Return> quits."
-print ""
 
-if len(sys.argv) > 1 :
-    cardset = sys.argv[1]
-    print "Using cards from set '" + cardset + "'"
-else :
-    cardset = "all"
+cardset = "all"
 
-cardfile = os.path.join(os.path.expanduser("~"), ".flashcards", cardset)
+cardfile = os.path.join(os.path.expanduser("~"), "flashcards", cardset)
+
 if (os.path.exists(cardfile)) :
     execfile(cardfile)
-else :
-    cardfile = os.path.join(os.path.expanduser("~"), "flashcards", cardset)
-    if (os.path.exists(cardfile)) :
-        execfile(cardfile)
                       
 if len(cards) > 0 :
     print "Read", len(cards), "cards from", cardfile
 
-print ""
+class StudyCard:
+    # "store a card and how well known it is."              1
+    def __init__(self, card):
+        self.myCard = card
+        self.skillLevel = 0
+        pass
 
-bonus_words = len(cards)
+    def show(self):
+        print_card(self.myCard)
+
+studyCards = []
+for card in cards:
+    studyCards.append(StudyCard(card))
+
+
+workingCards = []
+knownCards = []
+unknownCards=[]
+
+for card in studyCards:
+    unknownCards.append(card)
+
+workingCardCount = 5
+passingLevel = 3
+
+for index in range(0, workingCardCount):
+    card = random.choice(unknownCards)
+    workingCards.append(card)
+    unknownCards.remove(card)
+
 
 while True :
-    card = random.choice(cards)
-    which = random.choice(card)
+    card = random.choice(workingCards)
+    which = card.myCard[0]
 
     print which,
-    if raw_input() == "q" :
-        break
-    print_card(card)
 
+    ans = raw_input()
+    card.show()
     ans = raw_input()
     if ans == "q" :
         break
-    if ans != "" :
-        # Save another copy of this word in the list
-        cards.append(card)
-
-# Print the ones missed
-print "\nMissed:"
-for card in cards[bonus_words:] :
-    print_card(card)
+    if ans == "" :
+        card.skillLevel+=1
+        if card.skillLevel == passingLevel:
+            workingCards.remove(card)
+            knownCards.append(card)
+            print card.myCard[0], " moved to known."
+            if random.randint(0,1):
+                newCard = random.choice(knownCards)
+                knownCards.remove(newCard)
+                card.skillLevel-=1
+            else:
+                newCard = random.choice(unknownCards)
+                unknownCards.remove(newCard)
+            workingCards.append(newCard)
+    else:
+        card.skillLevel=0
 
